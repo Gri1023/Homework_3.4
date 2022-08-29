a8f4de25daa1b56640b14c94990889b17892cb66
Gri1023 <hryhorii.bavykin@gmail.com>
2022-08-29 14:18:17 +0300

    working!

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        int a = 6381;
-        while (a%387 != 0 && a%6381 != 0) {
+        int a = 1;
+        boolean found = false;
+        while (found==false) {
             if ((a % 387 == 0) && (a % 6381 == 0)) {
+                found = true;
                 System.out.println(a);
             }
-            else if (((a % 387 != 0) && (a % 6381 != 0))){
+            else {
                 a = a + 1;
-                System.out.println(a);
             }
         }
     }
